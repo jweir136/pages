71641dcc384e5d5097c84b5bbed78050d3ce5b8a
Jacob Weir <jweir136@yahoo.com>
2020-12-15 21:56:54 -0500

    add index

--- pages/pages/settings.py
+++ pages/pages/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp' # This is a dev-defined app
 ]
 
 MIDDLEWARE = [
